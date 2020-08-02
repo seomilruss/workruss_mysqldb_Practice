03b51a0257e900289a80395dcc7a7db7ae887208
seomilruss <54846678+seomilruss@users.noreply.github.com>
2020-08-02 16:26:47 +0900

    Add files via upload

---
+++ Exchange.sql
@@ -0,0 +1,16 @@
+/*
+   Check of your Database!
+   this query it's Mysql DataBase.
+   
+   CREATE DATABASE basic_calc;
+   DROP DATABASE basic_calc;
+   DROP TABLE exchange_value;
+   USE basic_calc;
+*/
+
+CREATE TABLE exchange_value (
+   won DOUBLE(100),
+   input_money INT(100)
+);
+
+SELECT * FROM exchange;
